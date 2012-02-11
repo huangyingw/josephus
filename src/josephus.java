034373ee9a942033c11f8c1b608932c5ca9981e8
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-11 16:07:22 +0800

    n

--- src/josephus.java
+++ src/josephus.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class josephus {
 	public static void main(String[] args) {
-		queue(10, 3);
+		System.out.println(queue(10, 3));
 	}
 
 	/**
@@ -12,41 +12,33 @@ public class josephus {
 	 *            总的人数
 	 * @param num
 	 *            第几号出队
+	 * @return
 	 */
-	public static void queue(int total, int num) {
-		// 定义一个数组，true表示没有出队列的，false表示已经出队列的
-		boolean[] arr = new boolean[total];
-		Arrays.fill(arr, true);
-		// 移动变量，如：1 2 3 1 2 3 1 2
-		int next = 1;
-		// 数组下标
-		int index = 0;
-		// 剩下的人数
+	public static int queue(int total, int num) {
 		int count = total;
-		// 如果剩下的人数为1人时，停止报数
+		int index = 0;
+		int next = 1;
+		boolean[] attrs = new boolean[total];
+		Arrays.fill(attrs, true);
 		while (count > 1) {
-			if (arr[index] == true) {
-				if (next == 3) {
-					arr[index] = false;
-					// 剩下的人数减1
-					--count;
-					// 移动变量复位，从1开始报数
+			if (attrs[index] == true) {
+				if (next == num) {
 					next = 1;
-					System.out.println("依次出列的人为：" + (index + 1));
+					attrs[index] = false;
+					count--;
 				} else {
-					++next;
+					next++;
 				}
 			}
-			++index;
-			if (index == total) {
-				// 数组下标复位，从0开始新重遍历
+			if (index == total - 1)
 				index = 0;
-			}
+			else
+				index++;
 		}
 		for (int i = 0; i < total; i++) {
-			if (arr[i] == true) {
-				System.out.println("最后出列的人为：" + (i + 1));
-			}
+			if (attrs[i] == true)
+				return i + 1;
 		}
+		return 0;
 	}
 }
